a95a857b4c4a87e94705e8a23dfca16340e2e2d1
PSriVignesH <srivigneshsri1999@gmail.com>
2024-03-27 10:53:49 +0530

    uninstalling sharp package and changing the next.config.mjs images from domains to remotepatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      "uploadthing.com",
-      "utfs.io"
-    ]
+  images:{
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname:'uploadthing.com'
+      },
+      {
+        protocol: 'https',
+        hostname:'utfs.io'
+      },
+    ],
   }
 };
 
 export default nextConfig;
+
